5367cc95cea736828823757472b32dfd9a3427d4
Kayla-Joubert <kayla@iiimpact.io>
2021-09-11 15:14:43 +0200

    adds working hashing functionality

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var crypto = require("crypto");
+const crypto = require("crypto");
 
-// The Power of a Smile
-// by Tupac Shakur
-var poem = [
+// The Power of a Smile - by Tupac Shakur
+const poem = [
 	"The power of a gun can kill",
 	"and the power of fire can burn",
 	"the power of wind can chill",
@@ -15,9 +14,73 @@ var poem = [
 	"especially yours can heal a frozen heart",
 ];
 
-var Blockchain = {
+const Blockchain = {
 	blocks: [],
 };
+
+const blockHash = (block) => {
+	return crypto
+		.createHash("sha256").update(
+			`${block.index}${block.prevHash}${block.data}${block.timestamp}`
+		).digest("hex");
+}
+
+const createBlock = (data, index) => {
+	let prevHash = Blockchain.blocks[index].hash;
+
+	let block = {
+		index: index + 1,
+		prevHash: prevHash,
+		data: data,
+		timestamp: Date.now(),
+	}
+
+	block.hash = blockHash(block)
+
+	Blockchain.blocks.push(block)
+
+	return block
+}
+
+const blockChecks = {
+	validHash: (genesisBlock, block) => {
+		if (genesisBlock && block.hash !== '000000') return false;
+		if (!genesisBlock) {
+			if (block.hash === '000000') return false;
+			if (block.hash !== blockHash(block)) return false;
+
+		}
+	},
+	validPrevHash: (genesisBlock, block) => {
+		if (!genesisBlock) {
+			if (block.prevHash.length < 0 || block.prevHash === "") return false;
+			if (block.prevHash !== Blockchain.blocks[block.index - 1].hash) return false;
+		}
+	},
+	validData: (block) => {
+		if (block.data.length < 0 && block.data !== "") return false;
+	},
+	validIndexblock: (block) => {
+		if (block.index >= 0 === false || !Number.isInteger(block.index)) return false;
+	}
+
+}
+
+const verifyBlock = (block) => {
+	const genesisBlock = Blockchain.blocks[0] === block;
+	blockChecks.validHash(genesisBlock, block)
+	blockChecks.validPrevHash(genesisBlock, block)
+	blockChecks.validData(block)
+	blockChecks.validIndexblock(block)
+
+	return true;
+}
+
+const verifyChain = (blockchain) => {
+	for (let block of blockchain.blocks) {
+		return verifyBlock(block)
+	}
+}
 
 // Genesis block
 Blockchain.blocks.push({
@@ -27,17 +90,8 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+poem.forEach((line, index) => {
+	createBlock(line, index)
+})
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-
-
-// **********************************
-
-function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
-}
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
